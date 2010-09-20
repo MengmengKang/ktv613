199b1680b7ac720f375d97757d750bcef3a581f4
dennis8911 <dennis8911@9afa9923-a374-7525-d188-b357c56b53d9>
2010-09-20 00:25:23 +0000

    git-svn-id: http://ktv613.googlecode.com/svn/trunk@19 9afa9923-a374-7525-d188-b357c56b53d9

---
+++ src/com/xch/db/dbConnect.java
@@ -0,0 +1,46 @@
+package cheng.xch.db;
+
+import java.sql.*;
+
+import cheng.xch.obj.User;
+
+public class dbConnect {
+	public static void addUser(User user)
+	{
+		//Scanner aS=new Scanner(System.in);
+		Connection aConnection;
+		Statement aStatement;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			aConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ktv613?useUnicode=true&characterEncoding=utf8","root","czx");  
+			aStatement= aConnection.createStatement();
+			String sql="drop table if exists admins";
+			aStatement.executeUpdate(sql);
+			sql="create table admins(Admin varchar(50),Password varchar(50))";
+			aStatement.executeUpdate(sql);
+		
+			System.out.println("Please Input User ID:");
+			sql="insert admins values('"+aS.next();
+			System.out.println("Please Input User Password:");
+			sql=sql+"','"+aS.next()+"')";
+			aStatement.executeUpdate(sql);
+			
+			sql="select * from admins";
+			ResultSet result=aStatement.executeQuery(sql);
+			System.out.println("ID	Password");
+			while(result.next()){
+				System.out.println(result.getString(1)+"	"+result.getString(2));		
+			}
+			aStatement.close();
+			aConnection.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+	
+
+	}
+}
+
+
