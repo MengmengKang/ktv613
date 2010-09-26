46f3b6380bfc316a4b45b9f26118463199eb1d56
dennis8911@gmail.com <dennis8911@gmail.com@9afa9923-a374-7525-d188-b357c56b53d9>
2010-09-26 15:22:26 +0000

    git-svn-id: http://ktv613.googlecode.com/svn/trunk@35 9afa9923-a374-7525-d188-b357c56b53d9

---
+++ src/com/xch/manage/SongAction.java
@@ -0,0 +1,18 @@
+package com.xch.manage;
+
+import com.xch.obj.SongData;
+
+public class SongAction extends SongData{
+	public void addSong(SongData Song)
+	{
+		
+	}
+	public void deleteSong(SongData Song)
+	{
+		
+	}
+	public void modifySong(SongData Song)
+	{
+		
+	}	
+}
---
+++ src/com/xch/order/SongSelect.java
@@ -0,0 +1,22 @@
+package com.xch.order;
+
+import com.xch.obj.SongData;
+
+public class SongSelect extends SongData{
+	public void selectBySongName(String SongName)
+	{
+		
+	}
+	public void selectBySongStar(String StarName)
+	{
+		
+	}
+	public void selectBySoPinYin(String PinYin)
+	{
+		
+	}
+	public void selectBySoNumber(int Number)
+	{
+		
+	}
+}
