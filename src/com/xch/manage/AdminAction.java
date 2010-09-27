6f29a2390437e5484b52d386aadabc6f653a3b85
dennis8911 <dennis8911@9afa9923-a374-7525-d188-b357c56b53d9>
2010-09-27 00:22:25 +0000

    git-svn-id: http://ktv613.googlecode.com/svn/trunk@40 9afa9923-a374-7525-d188-b357c56b53d9

---
+++ src/com/xch/manage/AdminAction.java
@@ -0,0 +1,22 @@
+package com.xch.manage;
+
+import com.xch.obj.AdminData;
+
+public class AdminAction extends AdminData{
+	public void addAdmin(AdminData Admin)
+	{
+		
+	}
+	public void deleteAdmin(AdminData Admin)
+	{
+		
+	}
+	public void modifyAdmin(AdminData Admin)
+	{
+		
+	}
+	public void findAdmin()
+	{
+		
+	}
+}
--- src/com/xch/manage/StarAction.java
+++ src/com/xch/manage/StarAction.java
@@ -1,5 +1,22 @@
 package com.xch.manage;
 
-public class StarAction {
+import com.xch.obj.StarData;
 
+public class StarAction extends StarData{
+	public void addStar(StarData Star)
+	{
+		
+	}
+	public void deleteStar(StarData Star)
+	{
+		
+	}
+	public void updateStar(StarData Star)
+	{
+		
+	}
+	public void findStar()
+	{
+		
+	}
 }
