3247635b4623c980bd3d75694ac272e1da20a6e1
dennis8911@gmail.com <dennis8911@gmail.com@9afa9923-a374-7525-d188-b357c56b53d9>
2010-10-09 09:35:38 +0000

    git-svn-id: http://ktv613.googlecode.com/svn/trunk@63 9afa9923-a374-7525-d188-b357c56b53d9

--- lib/导入数据库.cmd
+++ lib/导入数据库.cmd
@@ -2,8 +2,8 @@
 echo 按任意键开始导入数据库...
 pause
 ::mysql
-::create database ktv613
-::exit
+::echo create database ktv613;
+::exit;
 mysql -h localhost -u root -p ktv613 < ..\ktv613.sql
 echo 导入完成
 pause
